1e11a734d167e2553764eebc0f0ab3079c5c4642
Luz Soto <cuentasalternas52@gmail.com>
2026-05-05 16:33:30 -0500

    actualizacion

--- server.js
+++ server.js
@@ -98,14 +98,25 @@ app.post("/login", async (req, res) => {
 app.get("/perfil", async (req, res) => {
   try {
     const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ error: "No autorizado" });
+    }
+
     const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("ID del token:", decoded.id);
 
     const result = await pool.query(
       "SELECT usuario, nombre, correo FROM usuarios WHERE id=$1",
       [decoded.id]
     );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
 
     res.json(result.rows[0]);
 
@@ -114,7 +125,6 @@ app.get("/perfil", async (req, res) => {
     res.status(401).json({ error: "Token inválido" });
   }
 });
-
 // =========================
 // 💬 CHAT IA
 // =========================
