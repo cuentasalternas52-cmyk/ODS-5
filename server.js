45595c200dde65827380561dcc5345802125cdb9
Luz Soto <cuentasalternas52@gmail.com>
2026-05-05 15:25:56 -0500

    backend listo render

--- server.js
+++ server.js
@@ -151,7 +151,10 @@ app.post("/chat", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ mensaje: "API VozSegura funcionando 🚀" });
+  res.json({
+    estado: "OK",
+    mensaje: "VozSegura API funcionando 🚀"
+  });
 });
 
 // =========================
