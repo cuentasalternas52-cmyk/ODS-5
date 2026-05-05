9f292d7587d6e8caa267dae4dff29f4a8385aa8c
Luz Soto <cuentasalternas52@gmail.com>
2026-05-05 16:07:36 -0500

    actualizacion

--- server.js
+++ server.js
@@ -98,11 +98,6 @@ app.post("/login", async (req, res) => {
 app.get("/perfil", async (req, res) => {
   try {
     const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      return res.status(401).json({ error: "No autorizado" });
-    }
-
     const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
